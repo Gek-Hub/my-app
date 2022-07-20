556789804a8b899c43535472f3b3b2102049521d
Gek <gek33cr@gmail.com>
2022-07-20 20:17:12 +0300

    rabotaem

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import React from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  console.log(count)
+  console.log(setCount)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* <h1>likes: {likes}</h1>
+      <button onClick={Like}>Like</button>
+      <button onClick={Dislike}>Dislike</button> */}
     </div>
   );
 }
